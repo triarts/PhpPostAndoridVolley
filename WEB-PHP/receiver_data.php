b9b027a85345c102e74984c0a9a38b22eab4eec4
triarts <31907356+triarts@users.noreply.github.com>
2018-09-08 23:14:16 +0800

    Update receiver_data.php

--- WEB-PHP/receiver_data.php
+++ WEB-PHP/receiver_data.php
@@ -4,7 +4,7 @@ if(isset($_POST["data_iot"]))
 	echo $_POST["data_iot"];
 	//echo "sukses";
 
-// nanti seteleah keterima masu masuk database lah masu di apain terserah
+// nanti seteleah keterima mau masuk database lah masu di apain terserah
 
 ?>
 
